30b26c9b4617c29db8a512b3a36bb1dd6f2be40e
aenassar2000 <40957433+aenassar2000@users.noreply.github.com>
2023-08-31 21:30:58 -0500

    31/8/2023 Part1

--- AEIN392__Is_Subsequence/Solution.java
+++ AEIN392__Is_Subsequence/Solution.java
@@ -1,7 +1,30 @@
 package AEIN392__Is_Subsequence;
 
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-       return false;
+    static public boolean isSubsequence(String s, String t) {
+
+        if((s.length() == 0) || ((s.length() == 0) && (t.length() == 0)))
+            return true;
+        else if(s.length() > t.length())
+            return false;
+
+        int sp = 0;
+
+        for(int i = 0; i < t.length(); i++){
+            if(t.charAt(i) == s.charAt(sp)){
+                sp++;
+                if(sp == s.length())
+                    break;
+            }
+        }
+        if(sp == s.length()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("abc", "ahbgdc"));
     }
 }
